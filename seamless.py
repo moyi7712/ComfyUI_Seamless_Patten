406f7fb19c52d4f7b9b1c7f5c66bd95c535748cb
Alkaid <1415190485@qq.com>
2024-06-18 18:19:25 +0000

    add register hook

--- seamless.py
+++ seamless.py
@@ -1,5 +1,52 @@
 import torch.nn as nn
 
+
+
+def vae_circular_hook_pre(module, input):
+    if hasattr(module, 'padding_mode'):
+        setattr(module, 'padding_mode', 'circular')
+
+
+def vae_circular_hook(module, input, output):
+    if hasattr(module, 'padding_mode'):
+        setattr(module, 'padding_mode', 'zeros')
+    
+    if hasattr(module, 'circular_pre_hook'):
+        getattr(module, 'circular_pre_hook').remove()
+        delattr(module, 'circular_pre_hook')
+    
+    if hasattr(module, 'circular_hook'):
+        getattr(module, 'circular_hook').remove()
+        delattr(module, 'circular_hook')
+
+
+
+def circular_hook_pre(module, input):
+    for layer in module.modules():
+        if isinstance(layer, nn.Conv2d):
+            setattr(layer, 'padding_mode', 'circular')
+
+def circular_hook(module, input, output):
+    time_emb = input[1]
+
+    if time_emb>1:
+
+        return
+    for layer in module.modules():
+        if isinstance(layer, nn.Conv2d):
+            setattr(layer, 'padding_mode', 'zeros')
+
+    if hasattr(module, 'circular_pre_hook'):
+        getattr(module, 'circular_pre_hook').remove()
+        delattr(module, 'circular_pre_hook')
+    
+    if hasattr(module, 'circular_hook'):
+        getattr(module, 'circular_hook').remove()
+        delattr(module, 'circular_hook')
+
+
+
+
 class SeamlessApply:
     @classmethod
     def INPUT_TYPES(s):
@@ -9,6 +56,7 @@ class SeamlessApply:
                 "vae": ("VAE", ),
                 "active": ("BOOLEAN", {"default": True})
             }
+
         }
     
     RETURN_TYPES = ("MODEL", "VAE")
@@ -17,24 +65,28 @@ class SeamlessApply:
     CATEGORY = "Seamless"
 
     def apply(self, model, vae, active):
-        model = model.clone()
-        for layer in model.model.diffusion_model.modules():
-            if (isinstance(layer, nn.Conv2d)):
-                if active:
-                    layer.padding_mode = 'circular'
-                else:
-                    layer.padding_mode = 'zeros'
+        if not active:
+            return (model, vae)
 
+        model = model.clone()
+
+        unet_prehook = model.model.diffusion_model.register_forward_pre_hook(circular_hook_pre)
+        unet_hook = model.model.diffusion_model.register_forward_hook(circular_hook)
+        setattr(model.model.diffusion_model, 'circular_pre_hook', unet_prehook)
+        setattr(model.model.diffusion_model, 'circular_hook', unet_hook)
         
         patcher = vae.patcher.clone()
         for layer in patcher.model.modules():
             if (isinstance(layer, nn.Conv2d)):
-                if active:
-                    layer.padding_mode = 'circular'
-                else:
-                    layer.padding_mode = 'zeros'
+                pre_hook = layer.register_forward_pre_hook(vae_circular_hook_pre)
+                hook = layer.register_forward_hook(vae_circular_hook)
+                setattr(layer, 'circular_pre_hook', pre_hook)
+                setattr(layer, 'circular_hook', hook)
         vae.patcher = patcher
         vae.first_stage_model = patcher.model
         return (model, vae)
-        
+    
+    @classmethod       
+    def IS_CHANGED(s, **kwargs):
+        return float("nan")
 
